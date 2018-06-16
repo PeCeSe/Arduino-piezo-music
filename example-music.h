6ea192ed72f2fbdc8350555c59e757644e69b9fc
PeCeSe <PeCeSe@users.noreply.github.com>
2018-06-16 20:16:39 +0200

    Create example-music.h

---
+++ example-music.h
@@ -0,0 +1,142 @@
+
+// TWINKLE TWINKLE LITTLE STAR
+int twinkle_twinkle_melody[] = {
+  NOTE_C4, NOTE_C4, NOTE_G4, NOTE_G4, 
+  NOTE_A4, NOTE_A4, NOTE_G4,
+  NOTE_F4, NOTE_F4, NOTE_E4, NOTE_E4,
+  NOTE_D4, NOTE_D4, NOTE_C4
+};
+
+int twinkle_twinkle_rythm[] = {
+  4, 4, 4, 4, 
+  4, 4, 2,
+  4, 4, 4, 4, 
+  4, 4, 2
+};
+
+// ZELDA MAIN THEME
+int zelda_main_theme_melody[] = {
+  NOTE_C5, NOTE_G4, PAUSE, PAUSE, NOTE_C5, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, 
+  NOTE_G5, PAUSE, NOTE_G5, NOTE_G5, NOTE_GS5, NOTE_AS5, 
+  NOTE_C6, PAUSE, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_AS5, NOTE_GS5, 
+  NOTE_AS5, NOTE_AS5, NOTE_GS5, NOTE_G5, NOTE_G5, 
+  NOTE_F5, NOTE_F5, NOTE_G5, NOTE_GS5, NOTE_G5, NOTE_F5, 
+  NOTE_DS5, NOTE_DS5, NOTE_F5, NOTE_G5, NOTE_F5, NOTE_DS5, 
+  NOTE_D5, NOTE_D5, NOTE_E5, NOTE_FS5, NOTE_A5, 
+  NOTE_G5
+};
+
+int zelda_main_theme_rythm[] = {
+  4, 4, 8, 16, 16, 16, 16, 16, 16, 
+  2, 8, 8, 7, 7, 7, 
+  2, 8, 8, 8, 7, 7, 7, 
+  8, 16, 16, 2, 4, 
+  8, 16, 16, 2, 8, 8, 
+  8, 16, 16, 2, 8, 8,
+  8, 16, 16, 2, 4, 
+  1
+};
+
+// SUPER MARIO MAIN THEME
+// All credit to this article for this notation: 
+// https://www.princetronics.com/supermariothemesong/
+int mario_main_theme_melody[] = {
+  NOTE_E7, NOTE_E7, PAUSE, NOTE_E7,
+  PAUSE, NOTE_C7, NOTE_E7, PAUSE,
+  NOTE_G7, PAUSE, PAUSE,  PAUSE,
+  NOTE_G6, PAUSE, PAUSE, PAUSE,
+ 
+  NOTE_C7, PAUSE, PAUSE, NOTE_G6,
+  PAUSE, PAUSE, NOTE_E6, PAUSE,
+  PAUSE, NOTE_A6, PAUSE, NOTE_B6,
+  PAUSE, NOTE_AS6, NOTE_A6, PAUSE,
+ 
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, PAUSE, NOTE_F7, NOTE_G7,
+  PAUSE, NOTE_E7, PAUSE, NOTE_C7,
+  NOTE_D7, NOTE_B6, PAUSE, PAUSE,
+ 
+  NOTE_C7, PAUSE, PAUSE, NOTE_G6,
+  PAUSE, PAUSE, NOTE_E6, PAUSE,
+  PAUSE, NOTE_A6, PAUSE, NOTE_B6,
+  PAUSE, NOTE_AS6, NOTE_A6, PAUSE,
+ 
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, PAUSE, NOTE_F7, NOTE_G7,
+  PAUSE, NOTE_E7, PAUSE, NOTE_C7,
+  NOTE_D7, NOTE_B6, PAUSE, PAUSE
+};
+
+int mario_main_theme_rythm[] = {
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+ 
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+};
+
+// SUPER MARIO UNDERWORLD THEME
+// All credit to this article for this notation: 
+// https://www.princetronics.com/supermariothemesong/
+int mario_underworld_melody[] = {
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, PAUSE,
+  PAUSE,
+  NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+  NOTE_AS3, NOTE_AS4, PAUSE,
+  PAUSE,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, PAUSE,
+  PAUSE,
+  NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+  NOTE_DS3, NOTE_DS4, PAUSE,
+  PAUSE, NOTE_DS4, NOTE_CS4, NOTE_D4,
+  NOTE_CS4, NOTE_DS4,
+  NOTE_DS4, NOTE_GS3,
+  NOTE_G3, NOTE_CS4,
+  NOTE_C4, NOTE_FS4, NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,
+  NOTE_GS4, NOTE_DS4, NOTE_B3,
+  NOTE_AS3, NOTE_A3, NOTE_GS3,
+  PAUSE, PAUSE, PAUSE
+};
+
+int mario_underworld_rythm[] = {
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  3,
+  12, 12, 12, 12,
+  12, 12, 6,
+  6, 18, 18, 18,
+  6, 6,
+  6, 6,
+  6, 6,
+  18, 18, 18, 18, 18, 18,
+  10, 10, 10,
+  10, 10, 10,
+  3, 3, 3
+};
